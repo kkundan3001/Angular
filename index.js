15a2e4435ab790fa356fb88e03d4fd4e78d83e6f
Dev1 <Dev1@gmail.com>
2023-10-25 21:04:26 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+"use strict";
+let arr = ["raj", "taju", 20, 52, true];
+let arr1 = new Array();
+arr1[0] = 20;
+arr1[2] = "raj";
+arr1[3] = true;
+for (let ind in arr1) {
+    console.log(arr1[ind]);
+}
+//tuples
+const tp = [["taj", "ramu"], 8];
+console.log(tp[0]);
+console.log(tp[1]);
---
+++ index.ts
@@ -0,0 +1,12 @@
+let arr:(string|number|boolean)[]=["raj","taju",20,52,true];
+let arr1=new Array()
+  arr1[0]=20;
+ arr1[2]="raj"
+ arr1[3]=true
+ for(let ind in arr1){
+    console.log(arr1[ind])
+ }
+ //tuples
+ const tp:[string[],number]=[["taj","ramu"],8]
+console.log(tp[0])
+console.log(tp[1])
